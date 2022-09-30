edb510a514b5e190bfc5b1ba3e051b51e43dd8f8
JustaWandererTR <emir_han_ataman@hotmail.com>
2022-09-30 10:31:34 +0300

    Update test.py
    returning the total values to the function

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+from multiprocessing.resource_sharer import stop
+
+
 print("hi")
 print("This is a calculator app please hold on")
 total = 0
@@ -7,13 +10,18 @@ print("1 : +, 2 : -, 3 : *, 4 : /")
 symbol = input("What calculation do you want to do ? () :")
 print(Calculator(symbol))
 def Calculator(symbol):
+
     if(symbol == "+"):
         total = num1 + num2
+        return total
     elif(symbol == "-"):
         total == num1 - num2
+        return total
     elif(symbol == "*"):
         total == num1 * num2
+        return total
     elif(symbol == "/"):
         total == num1 / num2
+        return total
     
 
